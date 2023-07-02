9b05a21cccf0f4a8a3a681ff2b4b320a85e67bd5
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-07-02 02:30:12 -0300

    Update hello.asm

--- Assembly/hello-world/hello.asm
+++ Assembly/hello-world/hello.asm
@@ -1,10 +1,18 @@
 section .data
+        msg db 'Hello World!', 0xA
+        len equ $- msg
 
 section .text
 
 global _start
 
 _start:
+        mov     EAX, 0x4
+        mov     EBX, 0x1
+        mov     ECX, msg
+        mov     EDX, len
+        int     0x80
+
         mov     EAX, 0x1
         mov     EBX, 0x0
         int     0x80
